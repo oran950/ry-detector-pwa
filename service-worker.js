2f179a3424be42a53246e9ff1ca8b91021c6a274
Oran Ben David <43114098+oran950@users.noreply.github.com>
2025-05-03 18:54:46 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,40 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("app-cache").then((cache) => {
-        return cache.addAll([
-          "./",
-          "index.html",
-          "style.css",
-          "script.js",
-          "manifest.json",
-          "icon-192.png",
-          "icon-512.png",
-          "https://unpkg.com/wavesurfer.js"
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+const CACHE_NAME = 'audio-app-cache-v2'; // Change version when updating
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            console.log(`🗑 Deleting old cache: ${name}`);
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
